05c11839ddd86cbfa0f7fa47dc2cc97e3ecc74c9
Bill Sanderson <bill@towniebagels.com>
2025-05-22 09:40:12 -0700

    Made my section 12   # 125 updated code for male female icons and text

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-//import 'smallbox.dart';
-//import 'largebox.dart';
-// start  section 12 #125
- const bottomContainerHeight  = 80.0;
- const cardBackgroundColor = 0xFF1D1E33;
- const bottomContainerColor  = 0xFFEB1555;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() => runApp(BMICalculator());
+const bottomContainerHeight = 80.0;
+const cardBackgroundColor = 0xFF1D1E33;
+const bottomContainerColor = 0xFFEB1555;
+
+void main() => runApp(const BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   const BMICalculator({super.key});
@@ -32,38 +31,57 @@ class BMICalculator extends StatelessWidget {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: ReuseCard(colour: Color(cardBackgroundColor),)
+                    child: ReuseCard(
+                      colour: const Color(cardBackgroundColor),
+                      cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(FontAwesomeIcons.mars, size: 80, color: Colors.white),
+                          SizedBox(height: 15),
+                          Text('MALE', style: TextStyle(color: Colors.white, fontSize: 18)),
+                        ],
+                      ),
+                    ),
                   ),
                   Expanded(
-                    child: ReuseCard(colour: Color(cardBackgroundColor),
-                    )
+                    child: ReuseCard(
+                      colour: const Color(cardBackgroundColor),
+                      cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(FontAwesomeIcons.venus, size: 80, color: Colors.white),
+                          SizedBox(height: 15),
+                          Text('FEMALE', style: TextStyle(color: Colors.white, fontSize: 18)),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            //middle box on screen
-            Expanded(child: ReuseCard(colour: Color(cardBackgroundColor)),
+            Expanded(
+              child: ReuseCard(
+                colour: const Color(cardBackgroundColor),
+              ),
             ),
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child:ReuseCard(colour: Color(cardBackgroundColor))
+                    child: ReuseCard(colour: const Color(cardBackgroundColor)),
                   ),
                   Expanded(
-                    child: ReuseCard(colour: Color(cardBackgroundColor))
+                    child: ReuseCard(colour: const Color(cardBackgroundColor)),
                   ),
                 ],
               ),
             ),
-          // footer red bar
-          Container(
-            color: Color(bottomContainerColor ),
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: bottomContainerHeight,
-
-          )
+            Container(
+              color: const Color(bottomContainerColor),
+              margin: const EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            ),
           ],
         ),
       ),
@@ -72,18 +90,18 @@ class BMICalculator extends StatelessWidget {
 }
 
 class ReuseCard extends StatelessWidget {
-
   final Color colour;
-  ReuseCard({required this.colour});
+  final Widget? cardChild;
 
-
+  const ReuseCard({required this.colour, this.cardChild, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(15.0),
+      child: cardChild,
+      margin: const EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(cardBackgroundColor),
+        color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
